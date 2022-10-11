1b80172f65b21a8e678089e5c0a3018c8d233275
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-10-11 12:27:31 +0300

    Disease-field controller code

--- app/controllers/disease_fields_controller.rb
+++ app/controllers/disease_fields_controller.rb
@@ -1,2 +1,19 @@
 class DiseaseFieldsController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_response
+    
+    def create
+        disease_field = DiseaseField.create!(disease_field_params)
+        render json: disease_field, status: :created
+
+    end
+
+    private
+
+    def disease_field_params
+        params.permit(:disease_id, :field_id)
+    end
+
+    def unprocessable_entity_response(invalid)
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
 end
