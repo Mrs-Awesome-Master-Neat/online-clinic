a2f761a2c945a14644478eeef7d8c63d2cc9b756
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-10-11 14:12:20 +0300

    created users users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,50 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Guys are here!"
+
+NormalUser.create(
+    first_name: Faker::Name.male_first_name,
+    last_name: Faker::Name.last_name,
+    user_name:Faker::Internet.username,
+    date_of_birth: Date.new(1995,5,4),
+    gender: "Male",
+    email: Faker::Internet.email,
+    password: "123456man",
+    password_confirmation: "123456man",
+)
+NormalUser.create(
+    first_name: Faker::Name.female_first_name,
+    last_name: Faker::Name.last_name,
+    user_name:Faker::Internet.username,
+    date_of_birth: Date.new(1999,1,23),
+    gender: "Female",
+    email: Faker::Internet.email,
+    password: "123456babe",
+    password_confirmation: "123456babe",
+)
+puts "And the doctors"
+MedicalProfessionalUser.create(
+    first_name: Faker::Name.male_first_name,
+    last_name: Faker::Name.last_name,
+    user_name:Faker::Internet.username,
+    date_of_birth: Date.new(1976,8,6),
+    gender: "Male",
+    email: Faker::Internet.email,
+    password: "123456doc",
+    password_confirmation: "123456doc",
+    profession: "Medical Doctor",
+    started_work: Date.new(2007,3,5),
+    place_of_work: "Nairobi Hospital"
+)
+MedicalProfessionalUser.create(
+    first_name: Faker::Name.female_first_name,
+    last_name: Faker::Name.last_name,
+    user_name:Faker::Internet.username,
+    date_of_birth: Date.new(1990,11,12),
+    gender: "Female",
+    email: Faker::Internet.email,
+    password: "123456man",
+    password_confirmation: "123456man",
+    profession: "Nurse",
+    started_work: Date.new(2015,3,15),
+    place_of_work: "AAR Hospital"
+)
+puts "We are good!"
