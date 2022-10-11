e29ccf6ffab9db7e88a2d4c98844515f6be51275
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-10-11 12:33:53 +0300

    Code for disease controller

--- app/controllers/diseases_controller.rb
+++ app/controllers/diseases_controller.rb
@@ -1,2 +1,25 @@
 class DiseasesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index
+        diseases = Disease.all
+        render json: diseases, status: :ok
+    end
+
+    def show
+        disease = find_disease
+        render json: disease, status: :ok
+    end
+
+
+    private
+
+    def find_disease
+        Disease.find_by!(id: params[:id])
+
+    end
+
+    def render_not_found_response
+        render json: { error: "Disease not found" }, status: :not_found
+    end
 end
