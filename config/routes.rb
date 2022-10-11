fd1b3c02bd89a7ee03b947807c7f115f13cc02ea
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-10-11 15:02:32 +0300

    created routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  resources :users
   resources :disease_fields, only: [:create]
   resources :fields, only: [:index, :show]
   resources :diseases, only:[:index, :show]
+
+  get 'normal_users', to: "users#users_index"
+  get 'professionals', to: "users#professionals#index"
+  get 'normal_users/:id', to: "users#users_show"
+  get 'professionals/:id', to: "users#professionals_show"
+  post 'users', to: "users#create"
+  patch 'normal_users', to: "users#update_users"
+  patch 'professionals', to: "users#update_professionals"
+  patch 'users/upgrade', to: "users#upgrade"
 end
