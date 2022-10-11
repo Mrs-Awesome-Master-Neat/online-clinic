6af099948b7eb9c0ef37a2aa6cab016f033c7c30
Melyn Atieno Were <melynatieno@gmail.com>
2022-10-11 17:18:55 +0300

    Update serializers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,49 @@
 class PostsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index
+        render json: Post.all
+    end
+
+    def show
+        post = find_post
+        if post
+            render json: post
+        else
+            render_not_found_response
+        end
+    end
+
+    def create
+        post = Post.create!(post_params)
+        render json: post, status: :created
+    end
+
+    def update
+        post = find_post
+        post.update(post_params)
+        render json: post
+    end
+
+    def destroy
+        post = find_post
+        post.destroy
+        head :no_content
+    end
+
+    private
+    
+    def find_post
+        Post.find(params[:id])
+    end
+
+    def post_params
+        params.permit(:content)
+    end
+
+    def render_not_found_response
+        render json: {error: "Post not found"}, status: :not_found
+    end
+
 end
+
--- app/controllers/user_posts_controller.rb
+++ app/controllers/user_posts_controller.rb
@@ -1,3 +1,21 @@
 class UserPostsController < ApplicationController
-    
+    rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_response
+
+
+    def create
+        userpost = UserPost.create(userpost_params)
+        render json: userpost.post, serializer: UserPSerializer, status: :created
+    end
+
+        
+
+    private
+
+    def userpost_params
+        params.permit(:content)
+    end
+
+    def unprocessable_entity_response(invalid)
+        render json: {error: invalid.record.errors}, status: :unprocessable_entity
+    end
 end
