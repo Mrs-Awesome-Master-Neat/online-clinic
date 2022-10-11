a256cac18cd24b6aae68068866856367b755771a
Melyn Atieno Were <melynatieno@gmail.com>
2022-10-11 16:59:10 +0300

    update likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,20 @@
 class LikesController < ApplicationController
+
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index
+        render json: Like.all
+    end
+
+    def show
+        like = find_like
+        if like
+            render json: like
+        else
+            render_not_found_response
+        end
+    end
+
+ 
+
 end
