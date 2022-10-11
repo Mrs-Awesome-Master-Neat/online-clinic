4a8809ac59943bce4307e42a697f3ba28e353415
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-10-11 13:07:56 +0300

    Added subclasses for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_11_084636) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_11_092949) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -20,6 +20,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_084636) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "type"
+    t.string "user_name"
+    t.string "profession"
+    t.datetime "started_work"
+    t.string "place_of_work"
   end
 
 end
