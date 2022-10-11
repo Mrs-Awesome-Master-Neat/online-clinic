d90bc6c3ac60e55d7757be593ba72011c76b5cd3
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-10-11 12:09:26 +0300

    Creating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  resources :disease_fields
-  resources :fields
-  resources :diseases
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :disease_fields, only: [:create]
+  resources :fields, only: [:index, :show]
+  resources :diseases, only :[:index, :show]
 end
