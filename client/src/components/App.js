d680568f1a448e04c59b5c54beeaa7166b78c2d3
Melyn Atieno Were <melynatieno@gmail.com>
2022-10-12 12:36:47 +0300

    Update signup and login files

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,7 @@
 import '../style/App.css';
 import { Switch, Route, useHistory } from 'react-router-dom'
 import Login from './Login';
+import Signup from './Signup';
 
 function App() {
   const history = useHistory()
@@ -10,14 +11,14 @@ function App() {
         <Route path={"/login"}>
           <Login/>
         </Route>
-        <Route path={"/signin"}>
-             {/* <Signin/> */}
+        <Route path={"/signup"}>
+             <Signup/> 
         </Route>
   
         <Route exact path={"/"}>
           {/* create a button here to route to your component */}
           <button onClick={() => history.push("login")}>Login</button>
-          <button onClick={() => history.push("signin")}>signin</button>
+          <button onClick={() => history.push("signup")}>Signup</button>
         </Route>
       </Switch>
     </div>
