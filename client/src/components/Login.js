aee60069b29c7acb8e928b9435eaca041cced3e3
Melyn Atieno Were <melynatieno@gmail.com>
2022-10-12 21:39:41 +0300

    style login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,21 +1,27 @@
+import React from "react"
 import "../style/login.css"
+import image1 from "../images/hearth.png"
 
-export default function Login(){
+
+export default function Login() {
     return (
         <div className="login">
-            <div>
-                <img src="" alt="loginpic"/>
-            </div>
-            <div>
-                <p>Login</p>
-                <form className="login-form">
-                    <label>Email</label>
-                    <input id="email" type="text"/>
 
-                    <label>Password</label>
+            <img className="image" src={image1} alt="loginpic" />
+            <div>
+                <h1><b>WELCOME BACK!</b></h1>
+                <h1><b>Log In</b></h1>
+            </div>
+            <div className="form">
+
+                <form className="login-form">
+                    <label><b>Email</b></label>
+                    <input id="email" type="text" />
+
+                    <label><b>Password</b></label>
                     <input id="password" type="text" />
 
-                    <button className="loginbutton">LOG IN</button>
+                    <button className="loginbutton"><b>LOG IN</b></button>
                 </form>
 
             </div>
