748baacbdd11794ce6c8ec963f061a0b5e47c219
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-10-12 09:23:41 +0300

    frontend

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,56 +7,20 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <Switch>
-  //       {/* create a button here to route to your component */}
-        
-  //       <Route path={"/"}>
-
-  //       </Route>
-        
-  //     </Switch>
-  //   </div>
-  // );
-
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    // auto-login
-    fetch("/me").then((r) => {
-      if (r.ok) {
-        r.json().then((user) => setUser(user));
-      }
-    });
-  }, []);
-
   return (
     <div className="App">
-      <NavBar user={user} setUser={setUser} />
-      <main>
-        {user ? (
-          <Switch>
-            <Route path="/">
-              <Home user={user}/>
-            </Route>
-          </Switch>
-        ) : (
-          <Switch>
-            <Route path="/signup">
-              <SignUp setUser={setUser} />
-            </Route>
-            <Route path="/login">
-              <Login setUser={setUser} />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        )}
-      </main>
+      <Switch>
+        {/* create a button here to route to your component */}
+        
+        <Route path={"/"}>
+
+        </Route>
+        
+      </Switch>
     </div>
   );
+
+  
 }
 
 export default App;
