ebf3ca4a0d8982a4371daacbf438561268ecdfb6
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-10-12 09:20:29 +0300

    liveshare

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,60 @@
 import '../style/App.css';
-import {Switch,Route} from 'react-router-dom'
+import {Switch,Route} from 'react-router-dom';
+import {useState, useEffect} from 'react';
+import SignUp from "./SignUp";
+import Login from "./Login";
+import NavBar from "./NavBar";
+import Home from "./Home";
 
 function App() {
+  // return (
+  //   <div className="App">
+  //     <Switch>
+  //       {/* create a button here to route to your component */}
+        
+  //       <Route path={"/"}>
+
+  //       </Route>
+        
+  //     </Switch>
+  //   </div>
+  // );
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
-      <Switch>
-        {/* create a button here to route to your component */}
-        
-        <Route path={"/"}>
-
-        </Route>
-        
-      </Switch>
+      <NavBar user={user} setUser={setUser} />
+      <main>
+        {user ? (
+          <Switch>
+            <Route path="/">
+              <Home user={user}/>
+            </Route>
+          </Switch>
+        ) : (
+          <Switch>
+            <Route path="/signup">
+              <SignUp setUser={setUser} />
+            </Route>
+            <Route path="/login">
+              <Login setUser={setUser} />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        )}
+      </main>
     </div>
   );
 }
