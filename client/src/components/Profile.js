bc5ed671e6aa5e392d579b4e8ebc9de56f444d93
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-10-14 08:16:29 +0300

    no changes

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -26,7 +26,7 @@ function Profile() {
             </div>
         </div>
     
-    );
+    )
 }
 
 export default Profile;
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -81,7 +81,7 @@ function Signup({onLogin}) {
         </div>
 
 
-    );
+    )
 }
 
 export default Signup;
